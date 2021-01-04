e2b31e37648805b28407fda6dda983449f9360c6
Andrew Lane <AndrewLane@users.noreply.github.com>
2021-01-04 07:13:49 -0500

    Implementation (with tests) of points calculator (#4)
    * Implementation (with tests) of points calculator
    * Add the model of the Card
    * Establish our constants
    * Add our standard deck model
    * Add a failing test
    * Fixing Numbers on Bonus Points to Fix Bug
    
    Co-authored-by: IsaacLane <47743959+IsaacLane@users.noreply.github.com>

---
+++ NinetySixSimulator.ConsoleApp/Constants.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace NinetySixSimulator.ConsoleApp
+{
+    public static class Constants
+    {
+        public static class Suits
+        {
+            public const string Spades = "♠";
+            public const string Hearts = "♥";
+            public const string Diamonds = "♦";
+            public const string Clubs = "♣";
+        }
+
+        public static class GamePlayParameters
+        {
+            public static readonly TimeSpan TimeToPlayCard = TimeSpan.FromSeconds(1);
+            public static readonly TimeSpan TimeToShuffle = TimeSpan.FromSeconds(15);
+            public static readonly TimeSpan TimeToCountScore = TimeSpan.FromSeconds(60);
+            public static readonly TimeSpan TimeToPlayCardsFaceDownForAWar = TimeSpan.FromSeconds(3);
+        }
+
+        public const int NumberOfFaceDownCardsForWar = 3;
+    }
+}
---
+++ NinetySixSimulator.ConsoleApp/PointsCalculator.cs
@@ -0,0 +1,35 @@
+﻿using NinetySixSimulator.ConsoleApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NinetySixSimulator.ConsoleApp
+{
+    public class PointsCalculator
+    {
+        public int GetPoints(List<Card> cards)
+        {
+            int totalPoints = cards.Count();
+
+            foreach (var card in cards)
+            {
+                switch (card.Rank)
+                {
+                    case "A":
+                        totalPoints += 5;
+                        break;
+                    case "K":
+                        totalPoints += 3;
+                        break;
+                    case "Q":
+                        totalPoints += 2;
+                        break;
+                    case "J":
+                        totalPoints += 1;
+                        break;
+
+                }
+            }
+            return totalPoints;
+        }
+    }
+}
