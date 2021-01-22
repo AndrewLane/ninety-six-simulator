aca91459fa0c8d39b65001f07ad3f76b389b9269
Andrew Lane <AndrewLane@users.noreply.github.com>
2021-01-22 14:46:37 -0500

    Implementation of TrackIndividualGameState component (#20)
    * Implementation of TrackIndividualGameState component

---
+++ NinetySixSimulator.Services/ITrackIndividualGameState.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NinetySixSimulator.Services.Models;
+
+namespace NinetySixSimulator.Services
+{
+    public interface ITrackIndividualGameState
+    {
+        TimeSpan MaxDuration { get; }
+        PlayerGameState FirstPlayerState { get; set; }
+        PlayerGameState SecondPlayerState { get; set; }
+        bool TimedOut { get; }
+        TimeSpan TimeElapsed { get; }
+
+        ITrackIndividualGameState InitializeGameState();
+        void Tick(TimeSpan time);
+    }
+}
