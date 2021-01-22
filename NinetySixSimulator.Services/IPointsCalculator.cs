1cc1b5d81bacf94e368b773adc399ba10b3da354
Andrew Lane <andrew.lane@hobsons.com>
2021-01-22 14:49:18 -0500

    Add an interface for the PointsCalculator

---
+++ NinetySixSimulator.Services/IPointsCalculator.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using NinetySixSimulator.Services.Models;
+
+namespace NinetySixSimulator.Services
+{
+    public interface IPointsCalculator
+    {
+        int GetPoints(List<Card> cards);
+    }
+}
